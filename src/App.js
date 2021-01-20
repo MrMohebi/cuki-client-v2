4c0e33b94899d9c29b1864affab5aa1e381f2960
Mokafela <mkd0098@gmail.com>
2021-01-20 12:09:04 +0330

    BootStrap imported in appJs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter,Route} from "react-router-dom";
 import SplashScreen from "./Components/SplashScreen/SplashScreen";
-
+import 'bootstrap'
 
 
 function App() {
