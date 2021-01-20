65ae44dbf27d2b48e91982890b2f20abb863ce68
pooyson <ps4radical@gmail.com>
2021-01-20 23:34:18 +0330

    WelcomePage

---
+++ src/Components/WelcomePage/WelcomePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {AiOutlineHome, AiOutlineShoppingCart, AiOutlineHeart, AiOutlineUser} from 'react-icons/ai'
+import './css/style.css'
+import {blue} from "@material-ui/core/colors";
+class WelcomePage extends React.Component{
+    render() {
+        return(
+            <div >
+                <p className="welcomePageHeader">
+                    <span className="textColor">cuki </span>
+                    <span>  app</span>
+                </p>
+                <p className="welcomePageFrames">
+                    <span className="welcomePageDescriptionH">دوست داری چی بخوری؟</span>
+                    <br/>
+                    <span className="welcomePageDescription">اینجا میتونی غذاهای مورد علاقتو</span>
+                </p>
+
+            </div>
+
+        )
+    }
+}
+export default WelcomePage;
