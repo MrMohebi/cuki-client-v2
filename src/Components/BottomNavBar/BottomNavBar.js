efc42f711abe328b1f0a71738dce37c12f5e4488
Mokafela <mkd0098@gmail.com>
2021-01-22 09:33:33 +0330

    Unusable Navbar Codes Deleted and code optimized

--- src/Components/BottomNavBar/BottomNavBar.js
+++ src/Components/BottomNavBar/BottomNavBar.js
@@ -15,54 +15,65 @@ import userOut from '../../assets/img/navBar/userOut.png'
 import {IconButton} from '@material-ui/core';
 
 
-
-
-class BottomNavBar extends React.Component{
+class BottomNavBar extends React.Component {
     state = {
-        iconSize:10,
+        iconSize: 10,
     }
 
 
     render() {
-        return(
-            <div className='bottomNavBarMain' onContextMenu={(e)=>{e.preventDefault()}}>
-                <NavLink className='navLinkBottomNav' to={"/t"}  >
-                        {window.location.pathname === "/t" ?
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${cartFill})`}}/>
+        return (
+            <div className='bottomNavBarMain' onContextMenu={(e) => {
+                e.preventDefault()
+            }}>
+                <NavLink className='navLinkBottomNav' to={"/t"}>
+                    {window.location.pathname === "/t" ?
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${cartFill})`}}/>
                         :
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${cartOut})`}}/>
-                        }
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${cartOut})`}}/>
+                    }
                 </NavLink>
-                <NavLink className='navLinkBottomNav' to={"/t1"}  >
-                        {window.location.pathname === "/t1" ?
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${cartHisFill})`}}/>
+                <NavLink className='navLinkBottomNav' to={"/t1"}>
+                    {window.location.pathname === "/t1" ?
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${cartHisFill})`}}/>
                         :
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${cartHisOut})`}}/>
-                        }
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${cartHisOut})`}}/>
+                    }
                 </NavLink>
-                <NavLink className='navLinkBottomNav' to={"/t3"}  >
-                        {window.location.pathname === "/t3" ?
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${homeFill})`}}/>
+                <NavLink className='navLinkBottomNav' to={"/t3"}>
+                    {window.location.pathname === "/t3" ?
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${homeFill})`}}/>
                         :
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${homeOut})`}}/>
-                        }
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${homeOut})`}}/>
+                    }
                 </NavLink>
-                <NavLink className='navLinkBottomNav' to={"/t4"}  >
-                        {window.location.pathname === "/t4" ?
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${heartFill})`}}/>
+                <NavLink className='navLinkBottomNav' to={"/t4"}>
+                    {window.location.pathname === "/t4" ?
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${heartFill})`}}/>
                         :
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${heartOut})`}}/>
-                        }
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${heartOut})`}}/>
+                    }
                 </NavLink>
-                <NavLink className='navLinkBottomNav' to={"/t5"}  >
-                        {window.location.pathname === "/t5" ?
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${userFill})`}}/>
+                <NavLink className='navLinkBottomNav' to={"/t5"}>
+                    {window.location.pathname === "/t5" ?
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${userFill})`}}/>
                         :
-                            <IconButton className='BottomNavigationIcons'  style={{width:'40px',height:'40px',background:`url(${userOut})`}}/>
-                        }
+                        <IconButton className='BottomNavigationIcons'
+                                    style={{width: '40px', height: '40px', background: `url(${userOut})`}}/>
+                    }
                 </NavLink>
             </div>
         )
     }
 }
+
 export default BottomNavBar;
