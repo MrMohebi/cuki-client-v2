6bc5b74eaebc97b4b9967c0345cda6d040f5c0e6
Mokafela <mkd0098@gmail.com>
2021-01-28 01:25:38 +0330

    User Data Profile Front Finished

--- src/Components/ProfilePage/ProfilePage.js
+++ src/Components/ProfilePage/ProfilePage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './css/style.css'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 
@@ -8,8 +10,11 @@ class ProfilePage extends React.Component {
 
     state={
         History:true,
+        navActiveClass:'profileNavigate IranSans text-nowrap profileNavigateActive',
+        navNormalClass:'profileNavigate IranSans text-nowrap',
         UserTabClass:'profileNavigate IranSans text-nowrap',
-        HistoryTabClass:''
+        HistoryTabClass:'profileNavigate IranSans text-nowrap',
+
 
     }
     render() {
@@ -25,17 +30,35 @@ class ProfilePage extends React.Component {
                 </div>
                 <div className='profilePageContainer'>
                     <div className='w-100 d-flex justify-content-around pt-3'>
-                        <span className='profileNavigate IranSans text-nowrap'>کوکی کلاب</span>
+                        <span className={this.state.UserTabClass} onClick={arg=>{
+                            this.setState({
+                                HistoryTabClass:this.state.navNormalClass,
+                                UserTabClass:this.state.navActiveClass
+                            })
+                        }}>کوکی کلاب</span>
                         <span className='profileNavigate profileNavigateActive IranSans  text-nowrap invisible'> سفارش های من</span>
-                        <span className='profileNavigate IranSans  text-nowrap '> سفارش های من</span>
+                        <span className={this.state.HistoryTabClass} onClick={arg=>{
+                            this.setState({
+                                HistoryTabClass:this.state.navActiveClass,
+                                UserTabClass:this.state.navNormalClass
+                            })
+                        }}> سفارش های من</span>
                     </div>
-
-                    <div className='w-100 profileUserProfileContainer mt-5'>
-                        <div className='w-100 d-flex justify-content-between align-items-center pt-4'>
-                            <span className='historyOrderDate'>250T</span>
-                            <span className='historyOrderName'>اسپشیال فلان و فلان وفلان</span>
-                            <span className='historyOrderDate'>8/23</span>
+                    <div className='w-100 profileUserProfileContainer mt-3 d-flex flex-column '>
+                        <TextField className='rtl mt-2' id="standard-basic" label="اسم و فامیل" />
+                        <TextField className='rtl mt-2' id="standard-basic" label="تاریخ تولد" />
+                        <TextField className='rtl mt-2' id="standard-basic" label="شغل" />
+                        <div className='w-100 d-flex justify-content-center'>
+                            <div className='profileSubmitButton mt-4 IranSans'> ویرایش</div>
                         </div>
+                    </div>
+                    <div className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
+                        <span className='IranSans'>200</span>
+                        <span className='IranSans '>مجموع خرید </span>
+                    </div>
+                    <div className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
+                        <span className='IranSans'>200</span>
+                        <span className='IranSans'>امتیاز</span>
                     </div>
 
                 </div>
