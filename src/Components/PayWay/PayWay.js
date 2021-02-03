8e8a39ca8bdc769ee4f5c1a2e45736974fda87c9
Mokafela <mkd0098@gmail.com>
2021-02-03 15:32:29 +0330

    Pay Way Finished

--- src/Components/PayWay/PayWay.js
+++ src/Components/PayWay/PayWay.js
@@ -7,8 +7,45 @@ class PayWay extends React.Component{
         inResClass:'',
         onlineClass:'',
         offlineClass:'',
-        inOrOut: ''
+        inOrOut: 'out',
+        onlineOrCash:'cash'
     }
+    componentDidMount() {
+        if (this.state.inOrOut === 'in'){
+           this.enableInRes()
+        }else{
+            this.enableOutRes()
+        }
+        if (this.state.onlineOrCash === 'online'){
+            this.enableOnline()
+
+        }else{
+            this.enableCash()
+
+        }
+    }
+
+    enableInRes = ()=>{ this.setState({
+        inResClass:'payWayActive',
+        outResClass:'payWayNormal',
+        inOrOut:'in'
+    })}
+    enableOutRes = ()=>{this.setState({
+        inResClass:'payWayNormal',
+        outResClass:'payWayActive',
+        inOrOut:'out'
+
+    })}
+    enableOnline = ()=>{this.setState({
+        onlineClass:'payWayActive',
+        offlineClass:'payWayNormal',
+        onlineOrCash:'online'
+    })}
+    enableCash = ()=>{this.setState({
+        onlineClass:'payWayNormal',
+        offlineClass:'payWayActive',
+        onlineOrCash:'cash'
+    })}
     render(){
         return(
             <React.Fragment>
@@ -24,17 +61,18 @@ class PayWay extends React.Component{
                     <div className='payWayOptionsContainer d-flex flex-row-reverse justify-content-between'>
                         <span className='IranSans payWayText ' >نحوه پرداخت چجوری باشه؟</span>
                         <div className='payWayButtonsContainer d-flex flex-column justify-content-between'>
-                            <div className=' payWayActive'>آنلاین</div>
-                            <div className=' payWayNormal'>آنلاین</div>
-                            <div  > </div>
+                            <div className={this.state.onlineClass} onClick={this.enableOnline}
+                            >آنلاین</div>
+                            <div className={this.state.offlineClass} onClick={this.enableCash}>نقدی</div>
+
                         </div>
                     </div>
 
                     <div className='payWayOptionsContainer d-flex flex-row-reverse justify-content-between'>
                         <span className='IranSans payWayText ' >تحویل سفارش چطور؟</span>
                         <div className='payWayButtonsContainer d-flex flex-column justify-content-between'>
-                            <div className=' payWayActive'>درون رستوران</div>
-                            <div className=' payWayNormal'>بیرون بر</div>
+                            <div className={this.state.inResClass} onClick={this.enableInRes}>درون رستوران</div>
+                            <div className={this.state.outResClass} onClick={this.enableOutRes}>بیرون بر</div>
 
                         </div>
                     </div>
