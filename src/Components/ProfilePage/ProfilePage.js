9c438e4e65f4c16d170e6bc07c3902dbb60f9f45
Mokafela <mkd0098@gmail.com>
2021-02-05 21:08:59 +0330

    Date Picker Added and fixed

--- src/Components/ProfilePage/ProfilePage.js
+++ src/Components/ProfilePage/ProfilePage.js
@@ -5,95 +5,123 @@ import * as requests from '../../ApiRequests/ApiRequests.js'
 import * as actions from "../../stores/reduxStore/actions";
 import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 import {connect} from "react-redux";
-import { useSwipeable } from 'react-swipeable';
+import {useSwipeable} from 'react-swipeable';
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import DatePicker from 'react-modern-calendar-datepicker';
 
 export const Swipeable = ({children, style, ...props}) => {
     const handlers = useSwipeable(props);
-    return (<div style={style} { ...handlers }>{children}</div>);
+    return (<div style={style} {...handlers}>{children}</div>);
 }
 
 
 class ProfilePage extends React.Component {
+    state = {
+        activeProfile: 'history',
+        History: true,
+        navActiveClass: 'profileNavigate IranSans text-nowrap profileNavigateActive',
+        navNormalClass: 'profileNavigate IranSans text-nowrap',
+        UserTabClass: 'profileNavigate IranSans text-nowrap',
+        HistoryTabClass: 'profileNavigate IranSans text-nowrap',
+        clubElementClass: 'w-100',
+        historyElementClass: 'w-100 profileHistoryContainer mt-3 animate__animated animate__fadeOut d-none',
+        datePickerValue: {day: 9, month: 3, year: 1381},
+        inputsDisabled:true,
+        name:'Mohammad Elfi Poor',
+        birthday:564654,
+        job:'Rad shodan az divar'
+
+    }
+
     componentDidMount() {
-        if (this.props.match.params['part'] === 'club'){
+        if (this.props.match.params['part'] === 'club') {
             this.userTabClickHandler();
-        }else{
+        } else {
             this.historyTabClickHandler();
         }
-    }
-
-
-    state={
-        activeProfile:'history',
-        History:true,
-        navActiveClass:'profileNavigate IranSans text-nowrap profileNavigateActive',
-        navNormalClass:'profileNavigate IranSans text-nowrap',
-        UserTabClass:'profileNavigate IranSans text-nowrap',
-        HistoryTabClass:'profileNavigate IranSans text-nowrap',
-        clubElementClass:'w-100',
-        historyElementClass:'w-100 profileHistoryContainer mt-3 animate__animated animate__fadeOut d-none',
+        this.getUserInfo()
+        if (this.state.inputsDisabled){
+            this.disableDatePicker()
+        }
 
     }
 
-
-    userTabClickHandler = ()=>{
+    userTabClickHandler = () => {
         this.setState({
-            HistoryTabClass:this.state.navNormalClass,
-            UserTabClass:this.state.navActiveClass,
-            activeProfile:'club',
-            clubElementClass:'w-100 animate__animated animate__fadeIn',
-            historyElementClass:'w-100 profileHistoryContainer mt-3 animate__animated animate__fadeOut d-none',
+            HistoryTabClass: this.state.navNormalClass,
+            UserTabClass: this.state.navActiveClass,
+            activeProfile: 'club',
+            clubElementClass: 'w-100 animate__animated animate__fadeIn',
+            historyElementClass: 'w-100 profileHistoryContainer mt-3 animate__animated animate__fadeOut d-none',
 
         });
 
     }
 
-    historyTabClickHandler = ()=>{
+    historyTabClickHandler = () => {
         this.setState({
-            HistoryTabClass:this.state.navActiveClass,
-            UserTabClass:this.state.navNormalClass,
-            activeProfile:'his',
-            clubElementClass:'animate__animated animate__fadeOut d-none',
-            historyElementClass:'w-100 profileHistoryContainer mt-3 animate__animated animate__fadeIn',
+            HistoryTabClass: this.state.navActiveClass,
+            UserTabClass: this.state.navNormalClass,
+            activeProfile: 'his',
+            clubElementClass: 'animate__animated animate__fadeOut d-none',
+            historyElementClass: 'w-100 profileHistoryContainer mt-3 animate__animated animate__fadeIn',
         })
     }
 
-    getUserInfo = () =>{
+    getUserInfo = () => {
         requests.getUserInfo(this.callbackGetUserInfo)
         requests.getCustomerInfo(this.callbackGetCustomerInfo)
     }
 
-    callbackGetUserInfo = (res)=>{
-        if(res.hasOwnProperty("statusCode") && res.statusCode === 200){
+    callbackGetUserInfo = (res) => {
+        if (res.hasOwnProperty("statusCode") && res.statusCode === 200) {
             this.props.setUserData(res.data);
         }
     }
 
-    callbackGetCustomerInfo = (res)=>{
-        if(res.hasOwnProperty("statusCode") && res.statusCode === 200){
+    callbackGetCustomerInfo = (res) => {
+        if (res.hasOwnProperty("statusCode") && res.statusCode === 200) {
             this.props.setCustomerData(res.data);
         }
     }
 
-    swipeRight = () =>{
+    swipeRight = () => {
         this.userTabClickHandler()
-
     }
 
-    swipeLeft = () =>{
+    swipeLeft = () => {
         this.historyTabClickHandler()
-
+    }
+    enableDatePicker = ()=>{
+        document.getElementsByClassName('DatePicker')[0].style.pointerEvents = 'all'
+    }
+    disableDatePicker = ()=>{
+        document.getElementsByClassName('DatePicker')[0].style.pointerEvents = 'none'
     }
 
-    handleBack = () =>{
+    handleBack = () => {
         this.props.history.push("/main")
+    }
+    editAndApplyHandler = (e)=>{
+        if (this.state.inputsDisabled){
+            this.setState({
+                inputsDisabled:false
+            })
+            this.enableDatePicker()
+            e.target.innerHTML = "تایید"
+        }else{
+            this.setState({
+                inputsDisabled:true
+            })
+            this.disableDatePicker()
+            e.target.innerHTML = "ویرایش"
+        }
     }
 
     render() {
         return (
-            <Swipeable style={{height:"100%"}} onSwipedRight={this.swipeRight} onSwipedLeft={this.swipeLeft} children={
+            <Swipeable style={{height: "100%"}} onSwipedRight={this.swipeRight} onSwipedLeft={this.swipeLeft} children={
                 <React.Fragment>
-                    <div className='animate__animated'></div>
                     <div
                         className='categoryPageHeader pl-2 pr-2 pt-2 d-flex flex-row justify-content-between align-items-center'>
                         <ArrowBackRoundedIcon/>
@@ -112,6 +140,7 @@ class ProfilePage extends React.Component {
 
                         {/*{this.state.activeProfile === 'club' ? this.clubElement : this.historyElement}*/}
                         <div className={this.state.historyElementClass}>
+
                             <div className='w-100 d-flex justify-content-between align-items-center pt-4'>
                                 <span className='historyOrderDate'>250T</span>
                                 <span className='historyOrderName'>اسپشیال فلان و فلان وفلان</span>
@@ -120,36 +149,41 @@ class ProfilePage extends React.Component {
                         </div>
 
 
-
-
-
-
-
                         <div className={this.state.clubElementClass}>
                             <div className='w-100 profileUserProfileContainer mt-3 d-flex flex-column '>
-                                <TextField defaultValue={this.props.name} className='rtl mt-2 profileInputs' id="standard-basic" label="اسم و فامیل" />
-                                <TextField defaultValue={this.props.birthday} className='rtl mt-2 profileInputs' id="standard-basic" label="تاریخ تولد" />
-                                <TextField defaultValue={this.props.job} className='rtl mt-2 profileInputs' id="standard-basic" label="شغل" />
+                                <TextField disabled={this.state.inputsDisabled} defaultValue={this.state.name} className='rtl mt-2 profileInputs'
+                                           id="standard-basic" label="اسم و فامیل"/>
+                                <TextField disabled={this.state.inputsDisabled} defaultValue={this.state.birthday} className='rtl mt-2 profileInputs'
+                                           id="standard-basic" label="تاریخ تولد"/>
+                                <DatePicker
+                                    disabled
+                                    value={this.state.datePickerValue}
+                                    onChange={(e) => {
+                                        this.setState({
+                                            datePickerValue: e,
+                                        })
+                                    }}
+                                    shouldHighlightWeekends
+                                    locale="fa" // add this
+                                />
+
+                                <TextField disabled={this.state.inputsDisabled} defaultValue={this.state.job} className='rtl mt-2 profileInputs'
+                                           id="standard-basic" label="شغل"/>
                                 <div className='w-100 d-flex justify-content-center'>
-                                    <div className='profileSubmitButton mt-4 IranSans'> ویرایش</div>
+                                    <div className='profileSubmitButton mt-4 IranSans' onClick={this.editAndApplyHandler}> ویرایش</div>
                                 </div>
                             </div>
-                            <div className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
+                            <div
+                                className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
                                 <span className='IranSans'>{this.props.totalBoughtRestaurant / 1000} T</span>
                                 <span className='IranSans '>مجموع خرید </span>
                             </div>
-                            <div className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
+                            <div
+                                className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
                                 <span className='IranSans'>{this.props.scoreRestaurant}</span>
                                 <span className='IranSans'>امتیاز</span>
                             </div>
                         </div>
-
-
-
-
-
-
-
 
 
                     </div>
@@ -157,48 +191,19 @@ class ProfilePage extends React.Component {
             }/>
         )
     }
-
-
-    clubElement = <div className='w-100'>
-        <div className='w-100 profileUserProfileContainer mt-3 d-flex flex-column '>
-            <TextField defaultValue={this.props.name} className='rtl mt-2 profileInputs' id="standard-basic" label="اسم و فامیل" />
-            <TextField defaultValue={this.props.birthday} className='rtl mt-2 profileInputs' id="standard-basic" label="تاریخ تولد" />
-            <TextField defaultValue={this.props.job} className='rtl mt-2 profileInputs' id="standard-basic" label="شغل" />
-            <div className='w-100 d-flex justify-content-center'>
-                <div className='profileSubmitButton mt-4 IranSans'> ویرایش</div>
-            </div>
-        </div>
-        <div className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
-            <span className='IranSans'>{this.props.totalBoughtRestaurant / 1000} T</span>
-            <span className='IranSans '>مجموع خرید </span>
-        </div>
-        <div className='totalBuyAndPrice w-100 d-flex justify-content-between flex-row mt-3 pr-4 pl-4'>
-            <span className='IranSans'>{this.props.scoreRestaurant}</span>
-            <span className='IranSans'>امتیاز</span>
-        </div>
-    </div>
-
-
-    historyElement =   <div className='w-100 profileHistoryContainer mt-3'>
-        <div className='w-100 d-flex justify-content-between align-items-center pt-4'>
-            <span className='historyOrderDate'>250T</span>
-            <span className='historyOrderName'>اسپشیال فلان و فلان وفلان</span>
-            <span className='historyOrderDate'>8/23</span>
-        </div>
-    </div>
 }
 
 const mapStateToProps = (store) => {
     return {
-        token:store.rUserInfo.token,
-        name:store.rUserInfo.name,
-        birthday:store.rUserInfo.birthday,
-        job:store.rUserInfo.job,
-        totalBoughtAll:store.rUserInfo.totalBoughtAll,
-        totalBoughtRestaurant:store.rUserInfo.totalBoughtRestaurant,
-        orderTimesRestaurant:store.rUserInfo.orderTimesRestaurant,
-        scoreRestaurant:store.rUserInfo.scoreRestaurant,
-        lastOrderRestaurant:store.rUserInfo.lastOrderRestaurant,
+        token: store.rUserInfo.token,
+        name: store.rUserInfo.name,
+        birthday: store.rUserInfo.birthday,
+        job: store.rUserInfo.job,
+        totalBoughtAll: store.rUserInfo.totalBoughtAll,
+        totalBoughtRestaurant: store.rUserInfo.totalBoughtRestaurant,
+        orderTimesRestaurant: store.rUserInfo.orderTimesRestaurant,
+        scoreRestaurant: store.rUserInfo.scoreRestaurant,
+        lastOrderRestaurant: store.rUserInfo.lastOrderRestaurant,
     }
 }
 
