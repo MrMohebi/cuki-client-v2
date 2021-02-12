2eee02f72119208397483effd11cda108943792c
Mokafela <mkd0098@gmail.com>
2021-02-12 22:36:49 +0330

    Badged added to cart icon

--- src/Components/BottomNavBar/BottomNavBar.js
+++ src/Components/BottomNavBar/BottomNavBar.js
@@ -5,7 +5,7 @@ import 'animate.css/animate.css'
 import {connect} from "react-redux";
 
 
-import {IconButton} from '@material-ui/core';
+import {Badge, IconButton} from '@material-ui/core';
 
 import cf from './img/cartFill.png'
 import co from './img/cartOut.png'
@@ -29,56 +29,99 @@ class BottomNavBar extends React.Component {
                 <div className='bottomNavBarMain' onContextMenu={(e) => {
                     e.preventDefault()
                 }}>
-                    <NavLink className='navLinkBottomNav' to={"/bill"}>
-                        {window.location.pathname.includes("/bill") || window.location.pathname.includes("/payway") ?
-                            <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`, background:`url(${cf})`}}/>
-                            :
-                            <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${co})`}}/>
-                        }
-                    </NavLink>
+                    <Badge color='secondary' badgeContent={this.props.orderList.length}>
+                        <NavLink className='navLinkBottomNav' to={"/bill"}>
+                            {window.location.pathname.includes("/bill") || window.location.pathname.includes("/payway") ?
+                                <IconButton className='BottomNavigationIcons'
+                                            style={{
+                                                width: `${this.state.iconSize}`,
+                                                height: `${this.state.iconSize}`,
+                                                background: `url(${cf})`
+                                            }}/>
+                                :
+                                <IconButton className='BottomNavigationIcons'
+                                            style={{
+                                                width: `${this.state.iconSize}`,
+                                                height: `${this.state.iconSize}`,
+                                                background: `url(${co})`
+                                            }}/>
+                            }
+                        </NavLink>
+                    </Badge>
+
 
                     {
                         this.props.openOrdersList.length > 0 ?
                             <NavLink className='navLinkBottomNav' to={"/openOrders"}>
                                 {window.location.pathname.includes("/openOrders") ?
                                     <IconButton className='BottomNavigationIcons'
-                                                style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${chf})`}}/>
+                                                style={{
+                                                    width: `${this.state.iconSize}`,
+                                                    height: `${this.state.iconSize}`,
+                                                    background: `url(${chf})`
+                                                }}/>
                                     :
                                     <IconButton className='BottomNavigationIcons'
-                                                style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${cho})`}}/>
+                                                style={{
+                                                    width: `${this.state.iconSize}`,
+                                                    height: `${this.state.iconSize}`,
+                                                    background: `url(${cho})`
+                                                }}/>
                                 }
                             </NavLink>
                             :
-                                null
+                            null
                     }
 
                     <NavLink className='navLinkBottomNav' to={"/main"}>
                         {window.location.pathname.includes("/main") ?
                             <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${hf})`}}/>
+                                        style={{
+                                            width: `${this.state.iconSize}`,
+                                            height: `${this.state.iconSize}`,
+                                            background: `url(${hf})`
+                                        }}/>
                             :
                             <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${ho})`}}/>
+                                        style={{
+                                            width: `${this.state.iconSize}`,
+                                            height: `${this.state.iconSize}`,
+                                            background: `url(${ho})`
+                                        }}/>
                         }
                     </NavLink>
                     <NavLink className='navLinkBottomNav' to={"/likedFoods"}>
                         {window.location.pathname.includes("/likedFoods") ?
                             <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${hef})`}}/>
+                                        style={{
+                                            width: `${this.state.iconSize}`,
+                                            height: `${this.state.iconSize}`,
+                                            background: `url(${hef})`
+                                        }}/>
                             :
                             <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${heo})`}}/>
+                                        style={{
+                                            width: `${this.state.iconSize}`,
+                                            height: `${this.state.iconSize}`,
+                                            background: `url(${heo})`
+                                        }}/>
                         }
                     </NavLink>
                     <NavLink className='navLinkBottomNav' to={"/login"}>
                         {window.location.pathname.includes("/profile") ?
                             <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${uf})`}}/>
+                                        style={{
+                                            width: `${this.state.iconSize}`,
+                                            height: `${this.state.iconSize}`,
+                                            background: `url(${uf})`
+                                        }}/>
                             :
                             <IconButton className='BottomNavigationIcons'
-                                        style={{width:`${this.state.iconSize}`, height:`${this.state.iconSize}`,background:`url(${uo})`}}/>
+                                        style={{
+                                            width: `${this.state.iconSize}`,
+                                            height: `${this.state.iconSize}`,
+                                            background: `url(${uo})`
+                                        }}/>
                         }
                     </NavLink>
                 </div>
@@ -90,7 +133,7 @@ class BottomNavBar extends React.Component {
 
 const mapStateToProps = (store) => {
     return {
-        token:store.rUserInfo.token,
+        token: store.rUserInfo.token,
         orderList: store.rTempData.orderList,
         openOrdersList: store.rTempData.openOrdersList,
     }
