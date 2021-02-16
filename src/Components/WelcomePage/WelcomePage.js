476f13021f0190400adeee584d9208e84352d64a
Mokafela <mkd0098@gmail.com>
2021-02-16 15:22:33 +0330

    Welcome Page Bugs Fixed (scroll, onclick)

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -13,6 +13,7 @@ class WelcomePage extends React.Component {
     state = {
         handClass: "shakeHands",
         allowToShake: true,
+        partsPersianNames:{coffeeshop:'کافی شاپ',restaurant:'رستوران'}
     }
 
     componentDidMount() {
@@ -32,88 +33,91 @@ class WelcomePage extends React.Component {
     render() {
         return (
             <Swipeable style={{height: "100%"}}  onSwipedRight={this.swipeRight} onSwipedLeft={this.swipeLeft} children={
-                <div className="forLittlePhones">
-                    <p className="welcomePageHeader">
-                        <span className="textColor">Cuki </span>
-                        <span>  app</span>
-                    </p>
-                    <p className="welcomePageFrames1">
-                        <div className='HandAndHeaderText'>
-                            <span className="welcomePageDescriptionH"> دوست داری چی بخوری؟</span>
-                            <div onClick={(d) => {
-                                if (this.state.allowToShake) {
-                                    d.target['classList'].add("element")
-                                    this.setState({
-                                        allowToShake: false
-                                    })
-                                    setTimeout(() => {
-                                        d.target['classList'].remove("element")
+                <div className='welcomePageMainContainerCover w-100 h-100'>
+                    <div className="forLittlePhones">
+                        <p className="welcomePageHeader">
+                            <span className="textColor">Cuki </span>
+                            <span>  app</span>
+                        </p>
+                        <p className="welcomePageFrames1">
+                            <div className='HandAndHeaderText'>
+                                <span className="welcomePageDescriptionH"> دوست داری چی بخوری؟</span>
+                                <div onClick={(d) => {
+                                    if (this.state.allowToShake) {
+                                        d.target['classList'].add("element")
                                         this.setState({
-                                            allowToShake: true
+                                            allowToShake: false
                                         })
-                                    }, 1000)
-                                }
+                                        setTimeout(() => {
+                                            d.target['classList'].remove("element")
+                                            this.setState({
+                                                allowToShake: true
+                                            })
+                                        }, 1000)
+                                    }
 
-                            }} className={this.state.handClass} style={{
-                                background: 'url("./img/WelcomPage/shakehands.png")',
+                                }} className={this.state.handClass} style={{
+                                    background: 'url("./img/WelcomPage/shakehands.png")',
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    backgroundRepeat: 'no-repeat'
+                                }}/>
+                            </div>
+
+                            <div className="text-right">
+                                <span className="welcomePageDescription">اینجا میتونی غذاهای مورد علاقتو</span>
+                                <br/>
+                                <span className="welcomePageDescription">با بالاترین کیفیت ببینی و بعد انتخابش کنی</span>
+                                <br/>
+                                <span className="welcomePageDescription">حتی میتونی دونگی پرداخت کنی</span>
+                            </div>
+
+                            <div className="chefImage" style={{
+                                background: 'url("./img/WelcomPage/cheficon.png")',
                                 backgroundSize: 'cover',
                                 backgroundPosition: 'center',
                                 backgroundRepeat: 'no-repeat'
                             }}/>
-                        </div>
-
-                        <div className="text-right">
-                            <span className="welcomePageDescription">اینجا میتونی غذاهای مورد علاقتو</span>
                             <br/>
-                            <span className="welcomePageDescription">با بالاترین کیفیت ببینی و بعد انتخابش کنی</span>
+                        </p>
+                        <br/>
+                        <div className="welcomePageFrames2">
                             <br/>
-                            <span className="welcomePageDescription">حتی میتونی دونگی پرداخت کنی</span>
-                        </div>
+                            <div className="d-flex justify-content-around">
 
-                        <div className="chefImage" style={{
-                            background: 'url("./img/WelcomPage/cheficon.png")',
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                            backgroundRepeat: 'no-repeat'
-                        }}/>
-                        <br/>
-                    </p>
-                    <br/>
-                    <div className="welcomePageFrames2">
-                        <br/>
-                        <div className="d-flex justify-content-around">
+                                {this.props.foodListConverted.parts ? this.props.foodListConverted.parts.map(eachPart => {
+                                    return(
+                                        <div onClick={()=>{this.props.history.push("/category/"+eachPart)}} key={eachPart} className="openIcons">
+                                            <div className="burger" style={{
+                                                background: 'url("./img/resParts/'+eachPart+'.png")',
+                                                backgroundSize: '95%',
+                                                backgroundPosition: 'center',
+                                                backgroundRepeat: 'no-repeat',
+                                            }}/>
+                                            <span className="burgersAndDonatDescription" o>{this.state.partsPersianNames[eachPart]}</span>
+                                            <br/>
+                                            <br/>
+                                        </div>
+                                    )
+                                }): null
+                                }
 
-                            {this.props.foodListConverted.parts ? this.props.foodListConverted.parts.map(eachPart => {
-                                return(
-                                    <p key={eachPart} className="openIcons">
-                                        <div className="burger" style={{
-                                            background: 'url("./img/resParts/'+eachPart+'.png")',
-                                            backgroundSize: '95%',
-                                            backgroundPosition: 'center',
-                                            backgroundRepeat: 'no-repeat',
-                                        }}/>
-                                        <span className="burgersAndDonatDescription" onClick={()=>{this.props.history.push("/category/"+eachPart)}}>رستوران</span>
-                                        <br/>
-                                        <br/>
-                                    </p>
-                                )
-                            }): null
-                            }
+                                <div className="openIcons overflow-hidden">
+                                    <img src={tourImage} onClick={()=>(this.props.history.push('/vrTour'))} className='h-100 w-100 tourHolder'/>
+                                </div>
 
-                            <div className="openIcons overflow-hidden">
-                                <img src={tourImage} onClick={()=>(this.props.history.push('/vrTour'))} className='h-100 w-100 tourHolder'/>
+                                {/*<p className="openIcons overflow-hidden">*/}
+                                {/*    <div onClick={()=>(this.props.history.push('/vrTour'))} style={{background:`url(${tourImage})`,backgroundSize:'cover',backgroundPosition:'center'}} className='h-100 w-100 tourHolder'/>*/}
+                                {/*</p>*/}
+
+
+
+
                             </div>
-
-                            {/*<p className="openIcons overflow-hidden">*/}
-                            {/*    <div onClick={()=>(this.props.history.push('/vrTour'))} style={{background:`url(${tourImage})`,backgroundSize:'cover',backgroundPosition:'center'}} className='h-100 w-100 tourHolder'/>*/}
-                            {/*</p>*/}
-
-
-
-
                         </div>
                     </div>
                 </div>
+
             }/>
         )
     }
