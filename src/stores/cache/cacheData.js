19946b35e285c4b821adea478aad005542a8837d
MrM <MMMohebi@outlook.com>
2021-02-24 16:01:41 +0330

    cache phone

--- src/stores/cache/cacheData.js
+++ src/stores/cache/cacheData.js
@@ -2,11 +2,16 @@ import Cookies from "js-cookie";
 
 const cookieNameOrderList = "cukiOrderList_001210218"
 const cookieNameToken = "cukiToken_001210218"
+const cookieNamePhone = "cukiPhone_001210218"
 
 
 
 export const setCacheToken = (token) =>{
     Cookies.set(cookieNameToken, token)
+}
+
+export const setCachePhone = (phone) =>{
+    Cookies.set(cookieNamePhone, phone)
 }
 
 export const setCacheOrderList = (orderList) =>{
@@ -28,6 +33,9 @@ export const getCacheToken = () =>{
     return Cookies.get(cookieNameToken)
 }
 
+export const getCachePhone = () =>{
+    return Cookies.get(cookieNamePhone)
+}
 
 export const removeCacheToken = () =>{
     Cookies.remove(cookieNameToken)
