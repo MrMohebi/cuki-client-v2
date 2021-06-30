52ae3c722db18fbcd9ccba9fc49022ea864d5875
Mokafela <mkd0098@gmail.com>
2021-06-30 17:45:31 +0430

    Res details fixed
    TODO:
    fix the hours showing algo

--- src/Components/ResDetails/ResDetails.js
+++ src/Components/ResDetails/ResDetails.js
@@ -17,12 +17,15 @@ class ResDetails extends React.Component{
         resPhone:'',
         resAddress:'',
         resInstagram:'',
-        resWebsite:''
+        resWebsite:'',
+        logoLink:'',
+        openTimes:[[],[],[],[],[],[],[],]
     }
     componentDidMount() {
-        if(typeof this.state.resInfo !== "object"){
+
+        // if(typeof this.state.resInfo !== "object"){
             this.getResData()
-        }
+        // }
     }
 
     handleBack = ()=>{
@@ -30,11 +33,18 @@ class ResDetails extends React.Component{
     }
 
     getResData = () =>{
+
         requests.getRestaurantInfo((res)=>{
             if(res.hasOwnProperty("statusCode") && res.statusCode === 200){
+                console.log(res)
+                console.log(JSON.parse(res.data['openTime']))
                 ls.setLSResInfo(res.data)
                 this.setState({
                     resInfo:res.data,
+                    logoLink:res.data['logoLink'],
+                    resInstagram:res.data['socialLinks'],
+                    openTimes:JSON.parse(res.data['openTime'])
+
                 })
             }
         })
@@ -45,8 +55,7 @@ class ResDetails extends React.Component{
     render() {
         return (
             <React.Fragment>
-                <div
-                    className='categoryPageHeader  pl-2 pr-2 pt-2 d-flex flex-row justify-content-between align-items-center'>
+                <div className='categoryPageHeader  pl-2 pr-2 pt-2 d-flex flex-row justify-content-between align-items-center'>
                     <ArrowBackRoundedIcon onClick={this.handleBack}/>
                     <div className='categoryPageSelector text-center d-flex justify-content-around flex-row'>
                         <div className='categoryPageSelectorText IranSans'>اظلاعات رستوران</div>
@@ -54,7 +63,11 @@ class ResDetails extends React.Component{
                     <ArrowBackRoundedIcon className='invisible'/>
                 </div>
                 <div className='resDetailsContainer IranSansLight'>
-                    <section className={' w-100'}>
+                    <div className={'w-100 d-flex justify-content-center align-items-center'}>
+                        <img className={'res-details-logo'} src={this.state.logoLink} style={{opacity:this.state.logoLink?1:0,transition:'all 0.5s ease'}} alt=''/>
+                    </div>
+
+                    <section className={' w-100'} style={{transition:'all 0.2s ease',opacity:this.state.resPhone?1:0}}>
                         <div className={'mt-5 w-100 d-flex flex-row justify-content-around align-items-center'}>
                             <div className={'w-75 text-right'}>{this.state.resPhone}</div>
                                 <div className={'res-details-icons'} style={{background:`url(${phoneImage})`,backgroundSize:'cover'}}/>
@@ -84,9 +97,28 @@ class ResDetails extends React.Component{
                     <section>
 
                             <div>
-                                <div className={'w-100 text-right pr-3 mt-5 IranSans'} style={{fontSize:'1.5rem'}}>شروع کار مجموعه</div>
+                                <div className={'w-100 text-right pr-3 mt-5 IranSans'} style={{fontSize:'1.5rem'}}>ساعات کار مجموعه</div>
 
                             </div>
+                        <div className={'res-details-open-time-container rtl'}>
+                            <span className={'open-time-texts'}>شنبه از {this.state.openTimes[0][0]} تا {this.state.openTimes[0][this.state.openTimes[0].length-1]}</span>
+                            <span className={'open-time-texts'}>یک شنبه از {this.state.openTimes[1][0]} تا {this.state.openTimes[1][this.state.openTimes[1].length-1]}</span>
+                            <span className={'open-time-texts'}>دو شنبه از {this.state.openTimes[2][0]} تا {this.state.openTimes[2][this.state.openTimes[2].length-1]}</span>
+                            <span className={'open-time-texts'}>سه شنبه از {this.state.openTimes[3][0]} تا {this.state.openTimes[3][this.state.openTimes[3].length-1]}</span>
+                            <span className={'open-time-texts'}>چهار شنبه از {this.state.openTimes[4][0]} تا {this.state.openTimes[4][this.state.openTimes[5].length-1]}</span>
+                            <span className={'open-time-texts'}>پنج شنبه از {this.state.openTimes[5][0]} تا {this.state.openTimes[5][this.state.openTimes[5].length-1]}</span>
+                            <span className={'open-time-texts'}>جمعه از {this.state.openTimes[6][0]} تا {this.state.openTimes[6][this.state.openTimes[6].length-1]}</span>
+
+
+
+
+                            {/*<span className={'open-time-texts'}>یکشنبه از 0 تا 04</span>*/}
+                            {/*<span className={'open-time-texts'}>دوشنبه از 0 تا 04</span>*/}
+                            {/*<span className={'open-time-texts'}>سه شنبه از 0 تا 04</span>*/}
+                            {/*<span className={'open-time-texts'}>چهار شنبه از 0 تا 04</span>*/}
+                            {/*<span className={'open-time-texts'}>پنج شنبه از 0 تا 04</span>*/}
+                            {/*<span className={'open-time-texts'}>جمعه از 0 تا 04</span>*/}
+                        </div>
 
 
 
